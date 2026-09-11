aeacc1aaaf731074b577fa2f5a895988f3d8a7c0
Surya Prakash Pandey <2025578@imtnag.ac.in>
2026-09-11 20:31:56 +0530

    Update main.js

--- functions/job-orchestrator/src/main.js
+++ functions/job-orchestrator/src/main.js
@@ -1,1 +1,9 @@
+export async function main({ req, res, log, error }) {
+  log("Job Automation Orchestrator started");
 
+  return res.json({
+    status: "SUCCESS",
+    message: "Job Automation Orchestrator is running",
+    time: new Date().toISOString()
+  });
+}
